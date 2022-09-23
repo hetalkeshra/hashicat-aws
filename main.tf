103b8ad74e4312d3142521cb190500f1723c1ef5
Hetal Keshra <hetalkeshra26@gmail.com>
2022-09-23 06:30:35 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -127,7 +127,8 @@ resource "aws_instance" "hashicat" {
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
 
   tags = {
-    Name = "devops-hashicat-instance"
+    Name = "${var.prefix}-hashicat-instance"
+    Department = "devops"
     Billable = "true"
   }
 }
